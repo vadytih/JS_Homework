52da6005263a0f2d8198268960690c64dd847c5e
Vadim Tsikhinia <vady.tih@gmail.com>
2019-07-04 01:50:20 +0300

    hw-10

---
+++ Lesson-10/main.js
@@ -0,0 +1,46 @@
+function Animal(name) {
+    this.name = name;
+    this._foodAmount = 50;
+}
+    Animal.prototype._formatFoodAmount = function() {
+        return this._foodAmount + 'гр.';
+    };
+
+    Animal.prototype.dailyNorm = function (amount) {
+        if (!arguments.length) return this._formatFoodAmount();
+
+        if (amount < 50 || amount > 500) {
+            return 'Недопустимое количество корма.';
+        }
+
+        this._foodAmount = amount;
+    };
+
+    Animal.prototype.feed = function () {
+        console.log('Насыпаем в миску ' + this.dailyNorm() + ' корма.');
+    };
+
+function Cat(name) {
+    Animal.apply(this, arguments);
+}
+
+Cat.prototype = Object.create(Animal.prototype);
+Cat.prototype.constructor = Cat;
+
+
+Cat.prototype.feed = function () {
+    Animal.prototype.feed.apply(this);
+    console.log('Кот доволен ^_^');
+    return this;
+};
+
+Cat.prototype.stroke = function () {
+    console.log('Гладим кота.');
+    return this;
+};
+
+var barsik = new Cat('Барсик');
+
+console.log(barsik.feed().stroke().stroke().feed());
+
+barsik = null;
